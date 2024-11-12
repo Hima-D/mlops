3b6ed8b3fea06a6e4a45870521d7e17d304a39a3
Hima-D <dixithimanshu012@gmail.com>
2024-11-12 21:30:54 +0530

    req added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="mlproject",
+    version="0.0.1",
+    author="Himanshu",
+    author_email="hima-d@enack.in",
+    packages=find_packages(),
+    install_requires=[],
+)
